e7ba932d874c3c4a2b4262b5a5c66cb09699d020
Miguel Angel Montoya Londono <creasingfir2912@gmail.com>
2024-11-21 15:13:49 -0500

    Correcciones1

--- src/App.java
+++ src/App.java
@@ -8,48 +8,51 @@ public class App {
     static String[] naves = {"De reconocimiento", "Tripulada lenta", "Tripulada veloz"};
     static double[] velocidades = {15_000.0, 10_000.0, 20_000.0};
 
-
     public static void main(String[] args) throws Exception {
-      
     
-       int opcion;
-       do{
-            mostrarMenu();
-            opcion = scanner.nextInt();
-            switch (opcion) {
-                case 1:
-                    seleccionarPlaneta();
-                    break;
-                case 2:
-                    seleccionarNave();
-                    break;
-                case 3:
-                    seleccionarNave();
-                    break;
-                case 4:
-                    consultarRecursos();
-                    break;
-                case 5:
-                    iniciarSimulación();
-                    break;
-                case 6:
-                    
-                    break;
-                default:
-                    break;
+       getOpcion();
+
+    }
+
+    private static void getOpcion() {
+        int opcion;
+           mostrarMenu();
+        
+           
+            do{
+                opcion = scanner.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        seleccionarPlaneta();
+                        break;
+                    case 2:
+                       calcularDistanciaYTiempo();
+                        break;
+                    case 3:
+                        seleccionarNave();
+                        break;
+                    case 4:
+                        consultarRecursos();
+                        break;
+                    case 5:
+                        iniciarSimulación();
+                        break;
+                    case 6:
+                        
+                        break;
+                    default: System.err.println("Opción no válida");
+                        break;
 
 
 
 
 
-            }
+                }
 
 
 
-       }while(opcion != 4);
-
-
-
+            }while(opcion != 4);
     }
 
     public static void mostrarMenu(){
@@ -61,39 +64,34 @@ public class App {
         System.out.println("5. Iniciar la simulación del vuelo");
         System.out.println("6. Salir");
         System.out.println("Por favor, elige una opción: ");
+     
     }
 
-    public static void SeleccionarPlaneta(){
-
-        var lector = new Scanner(System.in);
+    public static void seleccionarPlaneta(){
 
         System.out.println("A continuación se encuentran los posibles destinos: ");
-        System.out.println("1. Mercurio");
+        System.out.println("1. Saturno");
         System.out.println("2. venus");
         System.out.println("3. marte");
-        System.out.println("4. jupiter");
-        System.out.println("5. saturno");
+
    
         System.out.println("A continuación seleccione su planeta de destino: ");
-        int opcion = scanner.nextInt();
+        int option = scanner.nextInt();
 
-       /*switch (option) {
+       switch (option) {
             case 1:
-                Mercurio();
+                System.out.printf("Planeta seleccionado: " + planetas[2] + "ditancia hasta su planeta es de " + distancias[2]);
                 break;
             case 2:
-                venus();
+                System.out.printf("Planeta seleccionado: " + planetas[1] + "ditancia hasta su planeta es de " + distancias[1]);
                 break;
             case 3:
-                marte();
+                System.out.printf("Planeta seleccionado: " + planetas[0] + "ditancia hasta su planeta es de " + distancias[0]);
                 break;
-            case 4:
-                jupiter();
+
+            default : System.err.println("Opción no válida");
                 break;
-            case 5:
-                saturno();
-                break;
-            }*/
+            }
 
     }
 
@@ -115,8 +113,6 @@ public class App {
     
 //Métodos auxiliares.
 public static void imprimirPlanetas(){}
-
-public static void seleccionarPlaneta(){}
 
 public static void lanzarEvento(){}
 
